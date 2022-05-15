b12f62bec570ae7793c6dcce5a849866de67a209
jung-ki-hun <khkh0130@gmail.com>
2022-05-16 03:15:49 +0900

    jkh add node mysql file add

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,8 +1,30 @@
 const express = require("express");
-
 const app = express.Router();
 
-app.use();
+var path = require('path');
+var static = require('serve-static');
+var session = require('express-session');
+var db = require('./db.js');
+var MySQLStore = require("express-mysql-session")(session);
+
+
+var db_info = db.getConnection();
+var sessionStore = new MySQLStore(db_info);
+app.use(
+	session({
+		key: "session_cookie_name#@",
+		secret: "session_cookie_secret",
+		store: sessionStore,
+		resave: false,
+		saveUninitialized: true,
+	})
+);
+app.use('/', router);
+app.use('/web', static(path.join(__dirname, 'web')));
+
+
 app.listen(80,()=>{
     console.log("http://localhost/");
 })
+
+
---
+++ nodejs/db.js
@@ -0,0 +1,34 @@
+const mysql = require('mysql');
+var argv_ip = process.argv[2]
+var db_info = {
+    host: argv_ip != '192.168.219.102' ? '180.83.98.144' : argv_ip.toString(), //'180.83.98.144' //'127.0.0.1'//'192.168.219.102'
+    port: '3306',
+    user: process.env.NAME_DB,
+    password: process.env.PASSWORD,
+    database: 'project'
+}
+
+module.exports = {
+    init: function () {
+        return mysql.createConnection(db_info);
+    },
+    connect: function (conn) {
+        conn.connect(function (err) {
+            if (err) console.error('mysql connection error : ' + err);
+            else console.log('mysql is connected successfully!');
+        });
+    },
+    clsose: function (conn) {
+        conn.disconnect(function (err) {
+            if (err) {
+                console.error('mysql connection error : ' + err);
+            }
+            else {
+                console.log('mysql connection disconnectting');
+            }
+        });
+    },
+    getConnection: function () {
+        return db_info;
+    }
+}
